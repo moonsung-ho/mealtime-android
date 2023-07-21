a139658ef06e1e8170b8501535ffb5c03227fe59
Woonki Moon <woonki.moon@gmail.com>
2023-07-22 02:05:58 +0900

    급식 메뉴 알러지 정보 파싱

--- app/src/main/java/com/sungho0205/geupsik/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sungho0205/geupsik/ui/home/HomeScreen.kt
@@ -9,10 +9,12 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.sungho0205.geupsik.Settings
 import com.sungho0205.geupsik.data.SettingsViewModel
+import com.sungho0205.geupsik.model.EAlergy
 import com.sungho0205.geupsik.ui.NavigationActions
 import java.text.SimpleDateFormat
 import java.time.Instant
@@ -98,11 +100,45 @@ fun HomeScreen(
                             modifier = Modifier.fillMaxWidth(),
                             elevation = CardDefaults.cardElevation(2.dp),
                         ) {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.SpaceBetween,
+                            Column(
+//                                verticalAlignment = Alignment.CenterVertically,
+//                                horizontalArrangement = Arrangement.SpaceBetween,
                             ) {
-                                Text(it.DDISH_NM)
+                                Text("종류: ${it.MMEAL_SC_NM}")
+                                Text("열량: ${it.CAL_INFO}")
+                                Spacer(modifier = Modifier.height(4.dp))
+
+                                val menus = it.DDISH_NM.split("<br/>")
+
+                                menus.map {
+                                    val regex =
+                                        Regex("[,.\\s]+") // Comma, period, or whitespace as delimiters.
+                                    val parts = it.split(regex).filter { it.isNotEmpty() }
+                                    val menu = parts[0]
+                                    val eAlergies = parts.drop(1).map {
+                                        EAlergy.values().find { eAlergy -> eAlergy.id == it }
+                                    }
+
+                                    val hasAlergy =
+                                        eAlergies.map { eAlergy -> eAlergy?.id }.intersect(
+                                            data.alergiesList.toSet().map { alergy -> alergy.id }
+                                                .toSet()
+                                        )
+                                            .isNotEmpty()
+
+                                    Text(
+                                        menu, color = if (hasAlergy) {
+                                            Color.Red
+                                        } else {
+                                            Color.Black
+                                        }
+                                    )
+                                    Column() {
+                                        eAlergies.map {
+                                            it?.label?.let { it1 -> Text(it1, color = Color.Gray) }
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
